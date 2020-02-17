93ae2b9751dda8dce74a915f40bceebb16949072
soohy <tngus90!>
2020-02-17 23:41:41 +0900

    [ADD] Restaurant validatopm

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +23,10 @@ public class Restaurant {
     @GeneratedValue
     private Long id;
 
+    @NotEmpty
     private String name;
+
+    @NotEmpty
     private String address;
 
     @Transient
--- eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
+++ eatgo-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/RestaurantControllerTest.java
@@ -95,7 +95,7 @@ class RestaurantControllerTest {
     }
 
     @Test
-    public void create() throws Exception {
+    public void createWithValidData() throws Exception {
         given(restaurantService.addRestaurant(any())).will(invocation -> {
             Restaurant restaurant = invocation.getArgument(0);
             return Restaurant.builder()
@@ -118,7 +118,17 @@ class RestaurantControllerTest {
     }
 
     @Test
-    public void update() throws Exception {
+    public void createWithInvalidData() throws Exception {
+        mvc.perform(
+                post("/restaurants")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"\", \"address\":\"\"}")
+        )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void updateWithValidData() throws Exception {
         mvc.perform(
                 patch("/restaurants/1004")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -127,6 +137,26 @@ class RestaurantControllerTest {
         .andExpect(status().isOk());
 
         verify(restaurantService).updateRestaurant(1004L, "JOKER Bar", "Busan");
+    }
+
+    @Test
+    public void updateWithInValidData() throws Exception {
+        mvc.perform(
+                patch("/restaurants/1004")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"\",\"address\":\"\"}")
+        )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void updateWithoutName() throws Exception {
+        mvc.perform(
+                patch("/restaurants/1004")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"\",\"address\":\"Busan\"}")
+        )
+                .andExpect(status().isBadRequest());
     }
 
 }
